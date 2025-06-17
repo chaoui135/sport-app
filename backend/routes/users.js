68b9ba833e97092cd11a4608fc90aeca2138c106
HAMZAOUI Ammar <ammar.hamzaoui@orange.com>
2025-06-17 14:06:42 +0200

    Migration de bcrypt vers bcryptjs pour compatibilité CI

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const User = require('../models/user');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
 const { v4: uuidv4 } = require('uuid');
